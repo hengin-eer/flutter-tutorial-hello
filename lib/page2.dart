7a2f5dbf3b6b8c2352fb7f6b59e1038c25e0c150
hengin-eer <hirokix0608@gmail.com>
2024-01-26 14:12:03 +0900

    ✨feat: PageViewの実装

--- lib/page2.dart
+++ lib/page2.dart
@@ -1,9 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:hello/testPage1.dart';
+import 'package:hello/testPage2.dart';
+import 'package:hello/testPage3.dart';
 
-class Page2 extends StatelessWidget {
-  const Page2({super.key});
+class Page2 extends StatefulWidget {
+  const Page2({super.key, required this.title});
 
-  // This widget is the root of your application.
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<Page2> createState() => _PageViewState();
+}
+
+class _PageViewState extends State<Page2> {
+  // @states here
+  late PageController _pageController;
+  int _selectedIndex = 0;
+
+  final _pages = [
+    TestPage1(),
+    TestPage2(),
+    TestPage3(),
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController = PageController(initialPage: _selectedIndex);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _pageController.dispose();
+  }
+
+  void _onPageChanged(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called, for instance as done
@@ -13,21 +59,21 @@ class Page2 extends StatelessWidget {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: const Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Icon(Icons.home),
-            Text("ホームです"),
-          ],
+        appBar: AppBar(
+          // Here we take the value from the MyHomePage object that was created by
+          // the App.build method, and use it to set our appbar title.
+          title: const Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(Icons.admin_panel_settings_sharp),
+              Text("PageView Sample"),
+            ],
+          ),
         ),
-      ),
-      body: const Center(
-          // Center is a layout widget. It takes a single child and positions it
-          // in the middle of the parent.
-          child: Text('hello, page2.')),
-    );
+        body: PageView(
+          controller: _pageController,
+          onPageChanged: _onPageChanged,
+          children: _pages,
+        ));
   }
 }
